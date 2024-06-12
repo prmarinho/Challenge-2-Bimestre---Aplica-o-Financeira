50e961f11ba8f26bfc1e7bb17bab2da1dd043312
Paulo R. Marinho <p7marinho@gmail.com>
2024-06-12 20:44:00 -0300

    first

---
+++ Financeiro/src/financeiro/Financeiro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package financeiro;
+
+/**
+ *
+ * @author paulo.rmarinho1
+ */
+public class Financeiro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
